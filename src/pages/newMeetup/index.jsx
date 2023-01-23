7683186af42319d27fa4279e8e0925e90463894c
Bruna Santos <brunahta2025@hotmail.com>
2023-01-23 17:59:58 -0300

    addMeetupHandler

---
+++ src/pages/newMeetup/index.jsx
@@ -0,0 +1,29 @@
+import NewMeetupForm from "@/components/meetups/NewMeetupForm"
+import { useRouter } from "next/router";
+
+
+ export default function NewMeetupPage(){
+    const router = useRouter();
+
+  async  function addMeetupHandler(enteredMeetupData){
+    const response = await fetch('/api/newMeetup', {
+        method: 'POST',
+        body: JSON.stringify(enteredMeetupData),
+        headers:{
+            'Content-Type': 'application/json',
+            
+        }
+    })
+        const data = await response.json()
+
+        console.log('enteredMeetupData', enteredMeetupData)
+        // console.log('data', data)
+        router.push('/')
+
+    }
+
+        return(
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
+            
+        )
+ }
